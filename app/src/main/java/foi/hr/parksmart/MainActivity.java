a84ff356b85a4f4139b5ada35e58c9d037f6663c
TKolar10 <tkolar@foi.hr>
2020-10-31 02:42:11 +0100

    Izrada početnog zaslona. Dodana je textView koji sadrži "Dobro došli!" i imageView koji sadrži logo aplikacije. Te je dodana provjera je li uređaj ima uključen bluetooth

--- app/src/main/java/foi/hr/parksmart/MainActivity.java
+++ app/src/main/java/foi/hr/parksmart/MainActivity.java
@@ -2,7 +2,11 @@ package foi.hr.parksmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String pozdrav = "Dobro došli";
+        TextView gritings = (TextView) findViewById(R.id.textView);
+        gritings.setText(pozdrav);
+        ImageView myImageView = (ImageView) findViewById(R.id.imageView);
+        myImageView.setImageResource(R.drawable.logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+                    if (!mBluetoothAdapter.isEnabled()) {
+                        String pozdravx="BLUETOOTH nije ukljucen ";
+                        TextView gritings = (TextView) findViewById(R.id.textView);
+                        gritings.setText(pozdravx);
+                    }
+                 }
+            },5000);
     }
 }
