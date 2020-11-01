744aa01fa3bbf99a5f4ea46a0f7d2d9113129f3e
Mario Duric <mduric@foi.hr>
2020-11-01 12:11:37 +0100

    Dodan prikaz automobila na main screen, zvuk on i zvuk off na klik gumba.

--- app/src/main/java/foi/hr/parksmart/MainScreen.java
+++ app/src/main/java/foi/hr/parksmart/MainScreen.java
@@ -3,6 +3,10 @@ package foi.hr.parksmart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+    }
+
+
+    public void hideButton(View view) {
+        FloatingActionButton BtnZvuk = (FloatingActionButton) findViewById(R.id.btnZvuk);
+        FloatingActionButton BtnZvukOff = (FloatingActionButton) findViewById(R.id.btnZvukOff);
+        BtnZvuk.setVisibility(View.GONE);
+        BtnZvukOff.setVisibility(View.VISIBLE);
+    }
+
+    public void showButton(View view) {
+        FloatingActionButton BtnZvuk = (FloatingActionButton) findViewById(R.id.btnZvuk);
+        FloatingActionButton BtnZvukOff = (FloatingActionButton) findViewById(R.id.btnZvukOff);
+        BtnZvukOff.setVisibility(View.GONE);
+        BtnZvuk.setVisibility(View.VISIBLE);
     }
 }
