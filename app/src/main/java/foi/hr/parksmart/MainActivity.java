0d0edc0d9a225df0c1288a1f31e7127b149b033b
TKolar10 <tkolar@foi.hr>
2020-11-03 18:29:59 +0100

    Napravljen je novi layout "bluetooth_message.xml" koji služi kao obavijest ukoliko uređaj nema spojen Bluetooth. Također je dodano ukoliko uređaj je spojen da preusmjeri na "Glavni zaslon".

--- app/src/main/java/foi/hr/parksmart/MainActivity.java
+++ app/src/main/java/foi/hr/parksmart/MainActivity.java
@@ -2,10 +2,15 @@ package foi.hr.parksmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String pozdrav = "Dobro došli";
-        TextView gritings = (TextView) findViewById(R.id.textView);
-        gritings.setText(pozdrav);
+        TextView greetings = (TextView) findViewById(R.id.textView);
+        greetings.setText(pozdrav);
         ImageView myImageView = (ImageView) findViewById(R.id.imageView);
         myImageView.setImageResource(R.drawable.logo);
 
@@ -28,28 +33,36 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (!mBluetoothAdapter.isEnabled()) {
+                    Dialog dialog = new Dialog(MainActivity.this);
+                    dialog.setContentView(R.layout.bluetooth_message);
+                    dialog.setTitle("Poruka");
+                    dialog.show();
 
-                    if (!mBluetoothAdapter.isEnabled()) {
-                        String pozdravx="BLUETOOTH nije ukljucen ";
-                        TextView gritings = (TextView) findViewById(R.id.textView);
-                        gritings.setText(pozdravx);
-                    }
-                 }
-            },5000);
 
-        /*
-        * Handler koji pokrece MainScreen activity
-        * 10 sekundi nakon pokretanja MainActivitya
-        */
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //pokretanje novog activitya
-                Intent intent = new Intent(MainActivity.this, MainScreen.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-                finish();
+                    do{
+                        Button btnOn = (Button) dialog.findViewById(R.id.btnTurnOn);
+                        btnOn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+
+                            public void onClick(View v) {
+                                final int REQUEST_ENABLE_BT = 1;
+                                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                            }
+                        });
+                    }while(mBluetoothAdapter.isEnabled());
+
+
+                }
+                else{
+                    Intent intent = new Intent(MainActivity.this, MainScreen.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    finish();
+                }
             }
-        },10000);
+        }, 1000);
+
     }
 }
