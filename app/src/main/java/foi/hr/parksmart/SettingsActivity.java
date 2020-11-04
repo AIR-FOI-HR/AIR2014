3524cc4ed9830c12f968169e045144b32fe42c4f
LAPTOP-2TDSH8BE\lukak <luka.klauzer9@gmail.com>
2020-11-04 16:12:34 +0100

    Revert "Napravljene male promjene na Settings view"
    This reverts commit c3057e9bd826afd2db62b128a7f5fff76962c558.

--- app/src/main/java/foi/hr/parksmart/SettingsActivity.java
+++ app/src/main/java/foi/hr/parksmart/SettingsActivity.java
@@ -20,7 +20,7 @@ public class SettingsActivity extends AppCompatActivity {
         }
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(false);
+            actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
 
