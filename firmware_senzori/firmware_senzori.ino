01db18fb0a1b6a85a1e9bed6e0a48d0c504d4028
LAPTOP-2TDSH8BE\lukak <luka.klauzer9@gmail.com>
2020-11-26 20:55:22 +0100

    Za razliku od prosle verzije, ESP razdvaja podatke za svaki senzor u posebnu varijablu

--- firmware_senzori/firmware_senzori.ino
+++ firmware_senzori/firmware_senzori.ino
@@ -3,8 +3,10 @@
 #define sensorSignal 4//6
 int minSignal = 100, maxSignal = 0;
 byte signal[5];
+String senzorA, senzorB, senzorC, senzorD;
 void GetSignalDigital()
 {
+  
   long int time = micros();
   while (digitalRead(sensorSignal) == HIGH)
   {
@@ -58,20 +60,28 @@ void setup()
 
 void loop()
 {
-  
+  senzorA=" ";senzorB=" ";senzorC=" ";senzorD=" ";
   if(pulseIn(sensorSignal,HIGH)>1000)
   {
-    for(int i=0;i<33;i++)signal[i]=pulseIn(sensorSignal,HIGH);
-    
+    //delay(1);
+    for(int i=0;i<9;i++)if(pulseIn(sensorSignal,HIGH)>150) senzorA+="1";else senzorA+=0;//signal[i]=pulseIn(sensorSignal,HIGH);
+    for(int i=0;i<8;i++)if(pulseIn(sensorSignal,HIGH)>150) senzorB+="1";else senzorB+=0;//signal[i]=pulseIn(sensorSignal,HIGH);
+    for(int i=0;i<8;i++)if(pulseIn(sensorSignal,HIGH)>150) senzorC+="1";else senzorC+=0;//signal[i]=pulseIn(sensorSignal,HIGH);
+    for(int i=0;i<8;i++)if(pulseIn(sensorSignal,HIGH)>150) senzorD+="1";else senzorD+=0;//signal[i]=pulseIn(sensorSignal,HIGH);
   }
-  for(int i=0;i<33;i++)Serial.println(signal[i]);
+  //for(int i=0;i<5;i++)Serial.println(signal[i]);
+  Serial.println(senzorA);
+  Serial.println(senzorB);
+  Serial.println(senzorC);
+  Serial.println(senzorD);
   Serial.println("--------------------------------------------");
+  char *charKey = new char[senzorA.length()];
+  senzorA.toCharArray(charKey, senzorA.length()+1);
+  long decKey = strtol(charKey, NULL, 2);
   
-  //  if (digitalRead(sensorSignal) == HIGH)Serial.print("1");
-  //  else {
-  //    Serial.print(" ");
-  //    Serial.println("0");
-  // }
+ 
+  
+
   //GetSignalDigital();
   //GetSignalAnalog();
   //Serial.println(analogRead(sensorSignal));
