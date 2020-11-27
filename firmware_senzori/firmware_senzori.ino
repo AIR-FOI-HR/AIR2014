3f309a1bdecb382196d7ad2c561cd1b554d8244e
LAPTOP-2TDSH8BE\lukak <luka.klauzer9@gmail.com>
2020-11-27 08:06:06 +0100

    Vrijednost svakog senzora se pohranjuje u string, zatim se pretvara u polje tipa char i na kraju pretvara u dekatski oblik koji ce se prosljediti mobilnoj aplikaciji

--- firmware_senzori/firmware_senzori.ino
+++ firmware_senzori/firmware_senzori.ino
@@ -4,9 +4,10 @@
 int minSignal = 100, maxSignal = 0;
 byte signal[5];
 String senzorA, senzorB, senzorC, senzorD;
+int senzorAdec, senzorBdec, senzorCdec, senzorDdec;
 void GetSignalDigital()
 {
-  
+
   long int time = micros();
   while (digitalRead(sensorSignal) == HIGH)
   {
@@ -32,7 +33,7 @@ void GetSignalDigital()
 void GetSignalAnalog()
 {
   long int time = micros();
-  while (analogRead(sensorSignal) >900)
+  while (analogRead(sensorSignal) > 900)
   {
   }
   //Serial.print("High: ");
@@ -41,7 +42,7 @@ void GetSignalAnalog()
 
   //Identify the length of the LOW signal---------------LOW
   time = micros();
-  while (analogRead(sensorSignal) <900)
+  while (analogRead(sensorSignal) < 900)
   {
   }
   //Serial.print("Low: ");
@@ -60,27 +61,46 @@ void setup()
 
 void loop()
 {
-  senzorA=" ";senzorB=" ";senzorC=" ";senzorD=" ";
-  if(pulseIn(sensorSignal,HIGH)>1000)
+  senzorA = " "; senzorB = " "; senzorC = " "; senzorD = " ";
+  if (pulseIn(sensorSignal, HIGH) > 1000)
   {
-    //delay(1);
-    for(int i=0;i<9;i++)if(pulseIn(sensorSignal,HIGH)>150) senzorA+="1";else senzorA+=0;//signal[i]=pulseIn(sensorSignal,HIGH);
-    for(int i=0;i<8;i++)if(pulseIn(sensorSignal,HIGH)>150) senzorB+="1";else senzorB+=0;//signal[i]=pulseIn(sensorSignal,HIGH);
-    for(int i=0;i<8;i++)if(pulseIn(sensorSignal,HIGH)>150) senzorC+="1";else senzorC+=0;//signal[i]=pulseIn(sensorSignal,HIGH);
-    for(int i=0;i<8;i++)if(pulseIn(sensorSignal,HIGH)>150) senzorD+="1";else senzorD+=0;//signal[i]=pulseIn(sensorSignal,HIGH);
-  }
+    delay(1);
+      for (int i = 0; i < 8; i++)if (pulseIn(sensorSignal, HIGH) > 150) senzorA += "1"; else senzorA += 0; //signal[i]=pulseIn(sensorSignal,HIGH);
+      for (int i = 0; i < 8; i++)if (pulseIn(sensorSignal, HIGH) > 150) senzorB += "1"; else senzorB += 0; //signal[i]=pulseIn(sensorSignal,HIGH);
+      for (int i = 0; i < 8; i++)if (pulseIn(sensorSignal, HIGH) > 150) senzorC += "1"; else senzorC += 0; //signal[i]=pulseIn(sensorSignal,HIGH);
+      for (int i = 0; i < 8; i++)if (pulseIn(sensorSignal, HIGH) > 150) senzorD += "1"; else senzorD += 0; //signal[i]=pulseIn(sensorSignal,HIGH);
   //for(int i=0;i<5;i++)Serial.println(signal[i]);
   Serial.println(senzorA);
   Serial.println(senzorB);
   Serial.println(senzorC);
   Serial.println(senzorD);
+
+  char *charKeyA = new char[senzorA.length()];
+  senzorA.toCharArray(charKeyA, senzorA.length()+ 1);
+  senzorAdec = strtol(charKeyA, NULL, 2);
+
+  char *charKeyB = new char[senzorB.length()];
+  senzorB.toCharArray(charKeyB, senzorB.length()+ 1);
+  senzorBdec = strtol(charKeyB, NULL, 2);
+
+  char *charKeyC = new char[senzorC.length()];
+  senzorC.toCharArray(charKeyC, senzorC.length() + 1);
+  senzorCdec = strtol(charKeyC, NULL, 2);
+
+  char *charKeyD = new char[senzorD.length()];
+  senzorD.toCharArray(charKeyD, senzorD.length() + 1);
+  senzorDdec = strtol(charKeyD, NULL, 2);
+  
+  Serial.println(senzorAdec/10);
+  Serial.println(senzorBdec/10);
+  Serial.println(senzorCdec/10);
+  Serial.println(senzorDdec/10);
   Serial.println("--------------------------------------------");
-  char *charKey = new char[senzorA.length()];
-  senzorA.toCharArray(charKey, senzorA.length()+1);
-  long decKey = strtol(charKey, NULL, 2);
+  }
   
- 
-  
+
+
+
 
   //GetSignalDigital();
   //GetSignalAnalog();
